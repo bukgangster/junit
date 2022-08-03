169b105a6d8db53349948ec0864f9998b3a76a84
limyangchae <limyangchae@kakao.com>
2022-08-03 11:07:46 +0900

    commit test

--- src/main/java/com/site/metacoding/junitproject/domain/Book.java
+++ src/main/java/com/site/metacoding/junitproject/domain/Book.java
@@ -1,5 +1,33 @@
 package com.site.metacoding.junitproject.domain;
 
-public class Book {
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
 
+import org.springframework.data.annotation.Id;
+
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@NoArgsConstructor
+@Getter
+@Entity
+public class Book {
+	
+	@GeneratedValue(strategy = GenerationType.IDENTITY )
+	@Id
+	private Long id;
+	private String title;
+	private String author;
+	
+	@Builder
+	public Book(Long id, String title, String author) {
+		super();
+		this.id = id;
+		this.title = title;
+		this.author = author;
+	}
+	
+	
 }
